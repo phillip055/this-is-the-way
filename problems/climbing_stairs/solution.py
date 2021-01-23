fe74e75c16cf524b8bc219e66518cfdca2d48904
phillip055 <vorathep055@gmail.com>
2021-01-23 16:59:38 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- problems/climbing_stairs/solution.py
+++ problems/climbing_stairs/solution.py
@@ -1,11 +1,13 @@
 from functools import lru_cache
+
 class Solution:
     @lru_cache(maxsize=32)
-    def climbStairs(self, n):
-        if n < 0:
+    def climbStairs(self, n: int) -> int:
+        if (n < 0):
             return 0
-        if n == 1:
+        if (n == 1):
             return 1
-        if n == 2:
+        if (n == 2):
             return 2
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        
+        return self.climbStairs(n - 1) + self.climbStairs(n-2)
