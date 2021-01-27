abec2b0961d4f64001145bef9f963814220fd921
Vorathep Sumetphong <vorathep055@gmail.com>
2021-01-27 15:31:33 +0000

    Sync LeetCode submission Runtime - 188 ms (17.75%), Memory - 16.7 MB (100.00%)

---
+++ problems/0136-single-number/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        s = set()
+        
+        for num in nums:
+            if (num in s):
+                s.remove(num)
+            else:
+                s.add(num)
+        
+        return list(s)[0]
+            
