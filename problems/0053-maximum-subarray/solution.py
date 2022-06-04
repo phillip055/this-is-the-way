4c9b8951ea1517d2bcaaae594fdf3845e104e18f
Vorathep Sumetphong <vorathep055@gmail.com>
2022-06-04 07:03:04 +0000

    Sync LeetCode submission Runtime - 1179 ms (5.02%), Memory - 27.9 MB (99.81%)

--- problems/0053-maximum-subarray/solution.py
+++ problems/0053-maximum-subarray/solution.py
@@ -1,15 +1,12 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_window = -math.inf
+        totalMax = float('-inf')
+        currentMax = float('-inf')
         
-        window_sum = 0
-        window_start = 0
-        
-        for window_end in range(len(nums)):    
-            window_sum += nums[window_end]
-            if window_sum > max_window:
-                max_window = window_sum
-            while (window_sum < 0):
-                window_sum -= nums[window_start]
-                window_start+=1
-        return max_window
+        for num in nums:
+            if (num > currentMax + num):
+                currentMax = num
+            else:
+                currentMax += num
+            totalMax = max(totalMax, currentMax)
+        return totalMax
