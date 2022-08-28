2a2fd0cd0faae5aa8fd3953888794ce6657c3464
Vorathep Sumetphong <vorathep055@gmail.com>
2022-08-28 09:08:56 +0000

    Sync LeetCode submission Runtime - 47 ms (6.59%), Memory - 13.8 MB (100.00%)

--- problems/0021-merge-two-sorted-lists/solution.py
+++ problems/0021-merge-two-sorted-lists/solution.py
@@ -4,13 +4,34 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if (l1 == None):
-            return l2
-        if (l2 == None):
-            return l1
-        
-        if (l1.val > l2.val):
-            return ListNode(l2.val, self.mergeTwoLists(l2.next, l1))
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        result = curr = ListNode()
+        while list1 and list2:
+            if list1.val > list2.val:
+                curr.next = list2
+                list2 = list2.next
+            else:
+                curr.next = list1
+                list1 = list1.next
+            curr = curr.next
+        if list1 != None:
+            curr.next = list1
         else:
-            return ListNode(l1.val, self.mergeTwoLists(l1.next, l2))
+            curr.next = list2
+        return result.next
+            
+# def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:     
+#         result = new = ListNode()
+#         while list1 and list2:
+#             if list1.val < list2.val:
+#                 new.next = list1
+#                 list1 = list1.next
+#             else:
+#                 new.next = list2
+#                 list2 = list2.next
+#             new = new.next
+#         if list1:
+#             new.next = list1
+#         else:
+#             new.next = list2
+#         return result.next
