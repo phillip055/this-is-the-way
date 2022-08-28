4f14b6035a64169a4f761ea52224ee4e6fd39ffd
Vorathep Sumetphong <vorathep055@gmail.com>
2022-08-28 08:24:10 +0000

    Sync LeetCode submission Runtime - 30 ms (5.05%), Memory - 13.2 MB (17.62%)

--- problems/0050-powx-n/solution.py
+++ problems/0050-powx-n/solution.py
@@ -1,13 +1,16 @@
-class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        if n<0:
-            x = 1/x
-            n = abs(n)
-        res = 1
-        while n > 0:
-            if n%2 == 1:
-                res *= x
-            x *= x
-            n //= 2
-        return res
-    
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        
+        if n == 0:
+            return 1
+        if n > 0:
+            if(n%2==0):
+                return self.myPow(x*x, n/2)
+            return x * self.myPow(x, n-1)
+        else:
+            return 1/ self.myPow(x, abs(n))
