3755fec91db39eaad715433cd0d61d82f9894616
Vorathep Sumetphong <vorathep055@gmail.com>
2022-10-24 18:16:36 +0000

    Sync LeetCode submission Runtime - 77 ms (10.59%), Memory - 14.4 MB (100.00%)

---
+++ problems/0150-evaluate-reverse-polish-notation/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        temp = []
+        while tokens:
+            token = tokens.pop(0)
+            if token not in ["*", "+", "/", "-"]:
+                temp.append(token)
+            else:
+                a = int(temp.pop())
+                b = int(temp.pop())
+                if token == "*":    
+                    res = a*b
+                if token == "/":
+                    res = int(b/a)
+                if token == "+":
+                    res = b+a
+                if token == "-":
+                    res = b-a
+                temp.append(res)
+        return temp[0]
+                    
