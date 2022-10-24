7d8d6f95a2731b0081fdd4f61af32b7a6db6e852
Vorathep Sumetphong <vorathep055@gmail.com>
2022-10-24 18:08:59 +0000

    Sync LeetCode submission Runtime - 137 ms (12.86%), Memory - 18.4 MB (100.00%)

--- problems/0155-min-stack/solution.py
+++ problems/0155-min-stack/solution.py
@@ -1,28 +1,30 @@
 class MinStack:
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.array = []
+        self.min_stack = []
 
-    def push(self, x: int) -> None:
-        self.array.append(x)
+    def push(self, val: int) -> None:
+        self.min_stack.append(
+            (val, min(self.getMin(), val))
+        )
+        return
 
     def pop(self) -> None:
-        self.array.pop()
-        
+        self.min_stack.pop()
+        return
 
     def top(self) -> int:
-        return self.array[-1]
+        return self.min_stack[-1][0]
 
     def getMin(self) -> int:
-        return min(self.array)
+        if len(self.min_stack) == 0:
+            return float('inf')
+        return self.min_stack[-1][1]
 
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
-# obj.push(x)
+# obj.push(val)
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
