5f2954888420c2871f8b23e32d020cb3ecc5729c
Vorathep Sumetphong <vorathep055@gmail.com>
2022-11-05 17:32:27 +0000

    Sync LeetCode submission Runtime - 33 ms (72.40%), Memory - 13.9 MB (100.00%)

---
+++ problems/0017-letter-combinations-of-a-phone-number/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        result = []
+        
+        d = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        
+        def p(current, i, digits):
+            if i >= len(digits):
+                if current != "":
+                    return result.append(current)
+            else:
+                for m in d[digits[i]]:
+                    p(current + m, i+1, digits)
+        p("", 0, digits)
+        return result
