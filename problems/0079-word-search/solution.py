495a079dc7fffa33570f87ac9625985e1d2dbdb2
Vorathep Sumetphong <vorathep055@gmail.com>
2022-11-05 10:25:08 +0000

    Sync LeetCode submission Runtime - 9187 ms (5.00%), Memory - 14 MB (100.00%)

--- problems/0079-word-search/solution.py
+++ problems/0079-word-search/solution.py
@@ -1,29 +1,35 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ans = False
-        # dp = [[-1 for val in range(len)]]
+        
+        def getNeighbors(board, i, j):
+            n = []
+            if i + 1 <= len(board) - 1:
+                n.append((i+1, j))
+            if i - 1 >= 0:
+                n.append((i-1, j))
+            if j + 1 <= len(board[0]) - 1:
+                n.append((i, j+1))
+            if j - 1 >= 0:
+                n.append((i, j-1))
+            return n
+        
+        def dfs(word, board, i, j, visited=set()):
+            if (i,j) in visited:
+                return False
+            if word == "":
+                return True
+            visited.add((i,j))
+            n = getNeighbors(board, i,j)
+            for e in n:
+                if board[e[0]][e[1]] == word[0] and dfs(word[1:], board, e[0], e[1], visited):
+                    return True
+            visited.remove((i,j))
+            return False
+                
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    int_ans = Solution.recur(board=board, word=word, ind=1, row=i, col=j)
-                    ans = ans or int_ans
-                    if ans == True:
-                        return True
-        return ans
-    
-    @staticmethod
-    def recur(board, word, ind, row, col):
-        if ind == len(word):
-            return True
-        
-        ans = False
-        char = board[row][col]
-        board[row][col] = '#'
-        for neigh in [[-1,0],[1,0],[0,-1],[0,1]]:
-            new_row = row + neigh[0]
-            new_col = col + neigh[1] 
-            if new_row >=0 and new_row < len(board) and new_col >=0 and new_col < len(board[0]):
-                if board[new_row][new_col] == word[ind]:
-                    ans = ans or Solution.recur(board, word, ind + 1, new_row, new_col)
-        board[row][col] = char
-        return ans
+                if word[0] == board[i][j] and dfs(word[1:], board, i, j):
+                    return True
+        return False
+                    
