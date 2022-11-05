03c9f37e3ab191207df1a875e4efaad803b751ce
phillip055 <vorathep055@gmail.com>
2022-11-05 17:24:06 +0000

    Sync LeetCode submission - Implement Trie (Prefix Tree) (python3)

---
+++ problems/implement_trie_(prefix_tree)/solution.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+class Trie:
+    table = {}
+    def __init__(self):
+        self.table = {}
+
+    def insert(self, word: str) -> None:
+        t = self.table
+        for c in word:
+            if c not in t:
+                t[c] = {}
+            t = t[c]
+        t['*'] = word
+
+    def search(self, word: str) -> bool:
+        t = self.table
+        for c in word:
+            if c in t:
+                t = t[c]
+            else:
+                return False
+        return "*" in t
+
+    def startsWith(self, prefix: str) -> bool:
+        t = self.table
+        for c in prefix:
+            if c in t:
+                t = t[c]
+            else:
+                return False
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
