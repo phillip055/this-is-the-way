f7cd1f097be807597ed2782f986019d3d73ad299
Vorathep Sumetphong <vorathep055@gmail.com>
2022-11-06 19:42:08 +0000

    Sync LeetCode submission Runtime - 57 ms (5.54%), Memory - 13.9 MB (100.00%)

--- problems/0070-climbing-stairs/solution.py
+++ problems/0070-climbing-stairs/solution.py
@@ -1,11 +1,8 @@
+from functools import cache
+
 class Solution:
-    def climbStairs(self, n: int, cache={}) -> int:
-        if n in cache:
-            return cache[n]
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        res = self.climbStairs(n-1) + self.climbStairs(n-2)
-        cache[n] = res
-        return res
+    @cache
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        return self.climbStairs(n-2) + self.climbStairs(n-1)
