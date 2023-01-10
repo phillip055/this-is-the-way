b47640aa5730a781b612f3fcde6218f1049fb6a6
phillip055 <vorathep055@gmail.com>
2023-01-10 05:30:22 +0000

    Sync LeetCode submission - Same Tree (python)

--- problems/same_tree/solution.py
+++ problems/same_tree/solution.py
@@ -1,18 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
         if p == None and q == None:
             return True
-        if p != None and q == None:
+        if ((p != None) ^ (q != None)) or p.val != q.val:
             return False
-        if q != None and p == None:
-            return False
-        if p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        else:
-            return False
+
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
+        return left and right
