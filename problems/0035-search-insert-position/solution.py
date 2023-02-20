2974f6768f21ee6282f2d5791a106dd94b9ce143
Vorathep Sumetphong <vorathep055@gmail.com>
2023-02-20 16:20:48 +0000

    Sync LeetCode submission Runtime - 47 ms (74.35%), Memory - 14.7 MB (100.00%)

--- problems/0035-search-insert-position/solution.py
+++ problems/0035-search-insert-position/solution.py
@@ -1,12 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            pivot = (left + right) // 2
-            if nums[pivot] == target:
-                return pivot
-            if target < nums[pivot]:
-                right = pivot - 1
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid - 1
             else:
-                left = pivot + 1
-        return left
+                start = mid + 1
+        return start
+
