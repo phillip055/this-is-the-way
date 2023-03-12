43e348695cc38fccb9adb2c33755303418a11bb0
Vorathep Sumetphong <vorathep055@gmail.com>
2023-03-12 00:37:49 +0000

    Sync LeetCode submission Runtime - 40 ms (45.36%), Memory - 13.9 MB (100.00%)

--- problems/0021-merge-two-sorted-lists/solution.py
+++ problems/0021-merge-two-sorted-lists/solution.py
@@ -5,33 +5,21 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        result = curr = ListNode()
-        while list1 and list2:
-            if list1.val > list2.val:
-                curr.next = list2
-                list2 = list2.next
+        head = ListNode(val=None)
+        cursor = head
+        cursor1 = list1
+        cursor2 = list2
+        while cursor1 and cursor2:
+            if cursor1.val < cursor2.val:
+                cursor.next = cursor1
+                cursor1 = cursor1.next
             else:
-                curr.next = list1
-                list1 = list1.next
-            curr = curr.next
-        if list1 != None:
-            curr.next = list1
-        else:
-            curr.next = list2
-        return result.next
-            
-# def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:     
-#         result = new = ListNode()
-#         while list1 and list2:
-#             if list1.val < list2.val:
-#                 new.next = list1
-#                 list1 = list1.next
-#             else:
-#                 new.next = list2
-#                 list2 = list2.next
-#             new = new.next
-#         if list1:
-#             new.next = list1
-#         else:
-#             new.next = list2
-#         return result.next
+                cursor.next = cursor2
+                cursor2 = cursor2.next
+            cursor = cursor.next
+        if cursor1:
+            cursor.next = cursor1
+        if cursor2:
+            cursor.next = cursor2
+        return head.next
+        
