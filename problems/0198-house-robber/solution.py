6a58de1ec1e50289686c3eb912ce067118c88320
Vorathep Sumetphong <vorathep055@gmail.com>
2023-03-18 04:10:32 +0000

    Sync LeetCode submission Runtime - 37 ms (49.35%), Memory - 14 MB (100.00%)

--- problems/0198-house-robber/solution.py
+++ problems/0198-house-robber/solution.py
@@ -1,15 +1,12 @@
 from functools import cache
 
-
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
         @cache
         def rec(i):
-            if i < 0:
+            if i > len(nums)-1:
                 return 0
-            
-            return max(rec(i-1), rec(i-2) + nums[i])
-        
-        return rec(len(nums) - 1)
+            return max(rec(i+1), rec(i+2) + nums[i])
+        return rec(0)
 
