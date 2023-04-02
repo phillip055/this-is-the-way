3efcb8e98fb2d4699ad321896d5873c487d7f038
Vorathep Sumetphong <vorathep055@gmail.com>
2023-04-02 13:37:44 +0000

    Sync LeetCode submission Runtime - 1159 ms (65.38%), Memory - 37.2 MB (99.34%)

---
+++ problems/2392-successful-pairs-of-spells-and-potions/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        answer = []
+
+        m = len(potions)
+        maxPotion = potions[m - 1]
+
+        for spell in spells:
+            minPotion = (success + spell - 1) // spell
+            if minPotion > maxPotion:
+                answer.append(0)
+                continue
+            index = bisect.bisect_left(potions, minPotion)
+            answer.append(m - index)
+
+        return answer
+
+        
+
