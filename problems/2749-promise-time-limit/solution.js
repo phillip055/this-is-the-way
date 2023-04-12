36e5b3653f1ad98d6ab349b1a3fb3025279746cc
Vorathep Sumetphong <vorathep055@gmail.com>
2023-04-12 18:30:34 +0000

    Sync LeetCode submission Runtime - 67 ms (18.12%), Memory - 42.2 MB (100.00%)

---
+++ problems/2749-promise-time-limit/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        const timeLimit = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject("Time Limit Exceeded")
+            }, t);
+        })
+        return Promise.race([timeLimit, fn(...args)])
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
