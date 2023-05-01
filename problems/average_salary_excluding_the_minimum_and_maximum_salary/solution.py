0ab57a36cdfe4b409bd61687c4e4426b00d0896f
phillip055 <vorathep055@gmail.com>
2023-05-01 04:56:02 +0000

    Sync LeetCode submission - Average Salary Excluding the Minimum and Maximum Salary (python3)

---
+++ problems/average_salary_excluding_the_minimum_and_maximum_salary/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        salary = sorted(salary)
+        salary.pop()
+        salary.pop(0)
+        return sum(salary) / len(salary)
