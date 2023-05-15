7c064548a19a6c64988916842791178665cfdc0f
Vorathep Sumetphong <vorathep055@gmail.com>
2023-05-15 16:32:03 +0000

    Sync LeetCode submission Runtime - 1294 ms (5.05%), Memory - 149.6 MB (6.11%)

--- problems/0528-swapping-nodes-in-a-linked-list/solution.py
+++ problems/0528-swapping-nodes-in-a-linked-list/solution.py
@@ -4,8 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-                
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         def length(head):
             if not head: return 0
             return 1 + length(head.next)
@@ -19,7 +18,3 @@ class Solution:
         temp1.next, temp2.next = temp2.next, temp1.next
         temp1.next.next, temp2.next.next = temp2.next.next, temp1.next.next
         return dummy.next
-
-        
-        
-        
