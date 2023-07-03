88e1ee253ccd0e46b53b7ea114a416ac7526aff0
Vorathep Sumetphong <vorathep055@gmail.com>
2023-07-03 02:58:31 +0000

    Sync LeetCode submission Runtime - 55 ms (6.43%), Memory - 16.6 MB (46.87%)

---
+++ problems/0889-buddy-strings/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        c1 = Counter(s)
+        c2 = Counter(goal)
+        if c1 != c2:
+            return False 
+        
+        diff = sum([1 for i in range(len(s)) if s[i] != goal[i]])
+        if diff == 2:
+            return True
+        elif diff == 0:
+            return any([cont > 1 for char, cont in c1.items()])
+        else:
+            return False
+
