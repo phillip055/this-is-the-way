594b5125963f1235153a7783b57bb5855eb267eb
Vorathep Sumetphong <vorathep055@gmail.com>
2023-07-23 11:03:28 +0000

    Sync LeetCode submission Runtime - 61 ms (6.55%), Memory - 16.6 MB (99.45%)

---
+++ problems/1603-running-sum-of-1d-array/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        result = [nums[0]]
+        for idx in range(1, len(nums)):
+            result.append(result[-1] + nums[idx])
+        return result
+            
