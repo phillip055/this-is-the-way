ac763aeac3e6f1772a88503dd30e344982c2c072
phillip055 <vorathep055@gmail.com>
2023-07-24 02:47:59 +0000

    Sync LeetCode submission - Pow(x, n) (python3)

--- problems/pow(x,_n)/solution.py
+++ problems/pow(x,_n)/solution.py
@@ -1,11 +1,5 @@
-class Solution(object):
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
         if n == 0:
             return 1
         if n > 0:
