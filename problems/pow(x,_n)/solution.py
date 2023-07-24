257822beba959f234f44704ea7bc82215fceeb79
phillip055 <vorathep055@gmail.com>
2023-07-24 10:14:01 +0000

    Sync LeetCode submission - Pow(x, n) (python3)

--- problems/pow(x,_n)/solution.py
+++ problems/pow(x,_n)/solution.py
@@ -3,8 +3,8 @@ class Solution:
         if n == 0:
             return 1
         if n > 0:
-            if(n%2==0):
-                return self.myPow(x*x, n/2)
-            return x * self.myPow(x, n-1)
+            if n % 2 == 0:
+                return self.myPow(x * x, n / 2)
+            return x * self.myPow(x, n - 1)
         else:
-            return 1/ self.myPow(x, abs(n))
+            return 1/ self.myPow(x, -1 * n)
