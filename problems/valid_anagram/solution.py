f519cf58d0b66b07c3ae2085e25cd003a512b1cf
phillip055 <vorathep055@gmail.com>
2023-12-16 02:42:13 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- problems/valid_anagram/solution.py
+++ problems/valid_anagram/solution.py
@@ -1,7 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_list = list(s)
-        t_list = list(t)
-        t_list.sort()
-        s_list.sort()
-        return t_list == s_list
+        return Counter(s) == Counter(t)
+        
