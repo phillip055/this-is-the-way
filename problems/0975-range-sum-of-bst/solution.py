e7e639b8d3dd3e062c55b0d11b9c959d036350ea
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-08 13:20:46 +0000

    Sync LeetCode submission Runtime - 132 ms (17.32%), Memory - 24.5 MB (5.28%)

--- problems/0975-range-sum-of-bst/solution.py
+++ problems/0975-range-sum-of-bst/solution.py
@@ -6,16 +6,9 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        def helper(node, low, high):
-            result = 0
-            if node == None:
-                return result
-            if node.val >= low and node.val <= high:
-                result += node.val
-            if node.left:
-                result += helper(node.left, low, high)
-            if node.right:
-                result += helper(node.right, low, high)
-            return result
-
-        return helper(root, low, high)
+        result = 0
+        if root is None:
+            return 0
+        if root.val >= low and root.val <= high:
+            result += root.val
+        return result + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
