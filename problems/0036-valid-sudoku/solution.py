e33e5375b9f06dcceb1d37eec9ef6eff73e8c398
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-20 17:19:31 +0000

    Sync LeetCode submission Runtime - 100 ms (17.41%), Memory - 16.7 MB (21.21%)

--- problems/0036-valid-sudoku/solution.py
+++ problems/0036-valid-sudoku/solution.py
@@ -1,16 +1,24 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rowSets = [set() for _ in range(9)]
         colSets = [set() for _ in range(9)]
+        rowSets = [set() for _ in range(9)]
         boxSets = [[set() for _ in range(3)] for _ in range(3)]
+
         for rowIdx in range(9):
             for colIdx in range(9):
                 val = board[rowIdx][colIdx]
+                rowBoxIdx = rowIdx // 3
+                colBoxIdx = colIdx // 3
                 if val == ".":
-                    continue
-                if val in rowSets[rowIdx] or val in colSets[colIdx] or val in boxSets[rowIdx//3][colIdx//3]:
+                     continue
+                if val in colSets[colIdx]:
                     return False
-                boxSets[rowIdx//3][colIdx//3].add(val)
-                rowSets[rowIdx].add(val)
+                if val in rowSets[rowIdx]:
+                    return False
+                if val in boxSets[rowBoxIdx][colBoxIdx]:
+                    return False
                 colSets[colIdx].add(val)
+                rowSets[rowIdx].add(val)
+                boxSets[rowBoxIdx][colBoxIdx].add(val)
         return True
+
