8e4a7a42aa77cdd31a0c82a05fe8ab234e93e987
phillip055 <vorathep055@gmail.com>
2024-01-20 16:45:13 +0000

    Sync LeetCode submission - Contains Duplicate (python3)

--- problems/contains_duplicate/solution.py
+++ problems/contains_duplicate/solution.py
@@ -1,3 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) != len(nums)
+        _set = set()
+        for num in nums:
+            if num in _set:
+                return True
+            _set.add(num)
+        return False
