d25bd2a9cbfbd268d5eec3f9d5e5628f091e790f
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-21 06:58:02 +0000

    Sync LeetCode submission Runtime - 26 ms (96.86%), Memory - 16.4 MB (89.65%)

---
+++ problems/0807-custom-sort-string/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        cnt = Counter(s)
+        ans = [cnt.pop(c, 0) * c for c in order]
+        for c, v in cnt.items():
+            ans.append(c * v)
+        return ''.join(ans)
+
