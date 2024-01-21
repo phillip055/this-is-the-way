08b81fd10d5d304f0a7963866a6db4804d964bb1
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-21 08:35:28 +0000

    Sync LeetCode submission Runtime - 30 ms (87.11%), Memory - 16.7 MB (6.01%)

--- problems/0050-powx-n/solution.py
+++ problems/0050-powx-n/solution.py
@@ -2,10 +2,13 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n == 0:
             return 1
+        if n == 1:
+            return x
         if n > 0:
             if n % 2 == 0:
-                return self.myPow(x * x, n / 2)
-            return x * self.myPow(x, n - 1)
+                return self.myPow(x * x, n//2)
+            else:
+                return x * self.myPow(x, n - 1)
         else:
-            return 1/ self.myPow(x, -1 * n)
+            return 1/self.myPow(x, -1*n)
 
