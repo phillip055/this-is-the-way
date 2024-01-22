65fffdb16a646a9e5d874de797cfd32235ba3e5a
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-22 14:00:47 +0000

    Sync LeetCode submission Runtime - 85 ms (88.86%), Memory - 16.6 MB (21.21%)

--- problems/0036-valid-sudoku/solution.py
+++ problems/0036-valid-sudoku/solution.py
@@ -3,22 +3,20 @@ class Solution:
         colSets = [set() for _ in range(9)]
         rowSets = [set() for _ in range(9)]
         boxSets = [[set() for _ in range(3)] for _ in range(3)]
-
-        for rowIdx in range(9):
-            for colIdx in range(9):
+        for rowIdx in range(len(board)):
+            for colIdx in range(len(board)):
                 val = board[rowIdx][colIdx]
-                rowBoxIdx = rowIdx // 3
-                colBoxIdx = colIdx // 3
+                rowBox = rowIdx // 3
+                colBox = colIdx // 3
                 if val == ".":
-                     continue
+                    continue
                 if val in colSets[colIdx]:
                     return False
                 if val in rowSets[rowIdx]:
                     return False
-                if val in boxSets[rowBoxIdx][colBoxIdx]:
+                if val in boxSets[rowBox][colBox]:
                     return False
                 colSets[colIdx].add(val)
                 rowSets[rowIdx].add(val)
-                boxSets[rowBoxIdx][colBoxIdx].add(val)
+                boxSets[rowBox][colBox].add(val)
         return True
-
