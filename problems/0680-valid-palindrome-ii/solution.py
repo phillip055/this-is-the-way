536d8d9a43536b9910e2efea8fea6632dd524883
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-22 11:20:35 +0000

    Sync LeetCode submission Runtime - 457 ms (5.01%), Memory - 220.2 MB (5.06%)

--- problems/0680-valid-palindrome-ii/solution.py
+++ problems/0680-valid-palindrome-ii/solution.py
@@ -1,14 +1,16 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        @cache
-        def helper(s, left, right, incorrectAllowed):
-            if left > right:
-                return True
-            if incorrectAllowed == -1:
-                return False
-            if s[left] == s[right]:
-                return helper(s, left + 1, right - 1, incorrectAllowed)
-            return helper(s, left + 1, right, incorrectAllowed - 1) or helper(s, left, right - 1, incorrectAllowed - 1)
         
-        return helper(s, 0, len(s) - 1, 1)
+        @cache
+        def helper(s, l, r, allowed_errors=1):
+            if l >= r:
+                return True
+            if s[l] == s[r]:
+                return helper(s, l+1, r-1, allowed_errors)
+            else:
+                if allowed_errors == 0:
+                    return False
+                allowed_errors -= 1
+                return helper(s, l+1, r, allowed_errors) or helper(s, l, r - 1, allowed_errors)
+        return helper(s, 0, len(s) - 1)
 
