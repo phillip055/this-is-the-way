31422e5e59a0d2ae10f209018b94d662e082b21b
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-23 16:40:19 +0000

    Sync LeetCode submission Runtime - 40 ms (24.40%), Memory - 16.7 MB (10.05%)

--- problems/1127-last-stone-weight/solution.py
+++ problems/1127-last-stone-weight/solution.py
@@ -3,7 +3,6 @@ class Solution:
         heap = [-stone for stone in stones]
         heapq.heapify(heap)
         while len(heap) > 1:
-            if (diff := -1*heapq.heappop(heap) + heapq.heappop(heap)):
-                heapq.heappush(heap, -diff)
-        return 0 if not heap else (-1 * heap[0])
-
+            diff = heappop(heap) - heappop(heap)
+            heappush(heap, diff)
+        return heap[0] * -1
