308015174cc124598b6e94be711c209e4f57e208
phillip055 <vorathep055@gmail.com>
2024-01-23 18:15:47 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- problems/climbing_stairs/solution.py
+++ problems/climbing_stairs/solution.py
@@ -1,7 +1,9 @@
-from functools import cache
 class Solution:
     @cache
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
         return self.climbStairs(n-1) + self.climbStairs(n-2)
+    
