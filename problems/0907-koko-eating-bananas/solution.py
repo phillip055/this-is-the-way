b429e71b457ef8d15dbad083b2df4ff964e01a3a
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-24 04:54:26 +0000

    Sync LeetCode submission Runtime - 254 ms (85.02%), Memory - 18.2 MB (14.20%)

--- problems/0907-koko-eating-bananas/solution.py
+++ problems/0907-koko-eating-bananas/solution.py
@@ -2,28 +2,17 @@ import math
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        
-        piles = sorted(piles)
+        def time_taken(piles, k):
+            return sum([math.ceil(pile / k) for pile in piles])
         
         l, r = 1, max(piles)
-
-
-        def calc(k, piles):
-            return sum(list(map(lambda x: math.ceil(x/k), piles)))
-
         ans = -1
-
         while l <= r:
-            mid = l + ((r-l)//2)
-            res = calc(mid, piles)
-            if res <= h:
+            mid = l + ((r-l) // 2)
+            if time_taken(piles, mid) <= h:
                 r = mid - 1
                 ans = mid
             else:
                 l = mid + 1
         return ans
-            
-
-            
-
 
