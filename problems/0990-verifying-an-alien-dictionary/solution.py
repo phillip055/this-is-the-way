6d9fbd47ff3f82845d365c0590cb9d7076aacdbd
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-24 06:17:52 +0000

    Sync LeetCode submission Runtime - 30 ms (97.37%), Memory - 16.6 MB (75.80%)

--- problems/0990-verifying-an-alien-dictionary/solution.py
+++ problems/0990-verifying-an-alien-dictionary/solution.py
@@ -1,19 +1,21 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        hash = {}
-        for i, e in enumerate(order):
-            hash[e] = i
-
-        def lessThan(w1, w2):
-            for e1, e2 in zip(w1, w2):
-                if e1 == e2:
+        if len(words) <= 1:
+            return True
+        order_dict = {}
+        for idx, o in enumerate(order):
+            order_dict[o] = idx
+        
+        def less_than_or_equal(word1, word2):
+            for l1, l2 in zip(word1, word2):
+                if order_dict[l1] == order_dict[l2]:
                     continue
-                return hash[e1] < hash[e2]
-            return len(w1) <= len(w2)
-        
+                return order_dict[l1] <= order_dict[l2]
+            return len(word1) <= len(word2)
+
         for idx in range(1, len(words)):
-            if not lessThan(words[idx - 1], words[idx]):
+            prev = words[idx - 1]
+            current = words[idx]
+            if not less_than_or_equal(prev, current):
                 return False
-        
         return True
-        
