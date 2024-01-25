10f217386da7c9dd0ec4b58d31d282e5d75712d9
phillip055 <vorathep055@gmail.com>
2024-01-25 05:17:55 +0000

    Sync LeetCode submission - Ransom Note (python3)

---
+++ problems/ransom_note/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNoteFreq = Counter(ransomNote)
+        magazineFreq = Counter(magazine)
+
+        for k, v in ransomNoteFreq.items():
+            if magazineFreq[k] < ransomNoteFreq[k]:
+                return False
+        return True
