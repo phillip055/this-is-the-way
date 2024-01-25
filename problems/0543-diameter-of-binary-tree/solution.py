b44c61930e50a016fd51ae07f77fcfc432937ee4
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-25 01:20:30 +0000

    Sync LeetCode submission Runtime - 42 ms (87.21%), Memory - 17.8 MB (100.00%)

--- problems/0543-diameter-of-binary-tree/solution.py
+++ problems/0543-diameter-of-binary-tree/solution.py
@@ -4,27 +4,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-class NodeInfo:
-    def __init__(self, maxDiameter, maxDepth):
-        self.maxDiameter = maxDiameter
-        self.maxDepth = maxDepth
-
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        def DBT(root):
-            if root:
-                p = DBT(root.left)
-                q = DBT(root.right)
-                
-                maxD = p.maxDepth + q.maxDepth
-                
-                
-                return NodeInfo(
-                    max(maxD, p.maxDiameter, q.maxDiameter),
-                    1 + max(p.maxDepth, q.maxDepth)
-                )
-            else:
-                return NodeInfo(0, 0)
-        return DBT(root).maxDiameter
-                    
+        
+        def helper(root):
+            if not root:
+                return 0, 0
+            l_max_path, l_max_diameter = helper(root.left)
+            r_max_path, r_max_diameter = helper(root.right)
+            diameter = l_max_path + r_max_path
+            path = 1 + max(l_max_path, r_max_path)
+            return path, max(l_max_diameter, r_max_diameter, diameter)
+        res = helper(root)
+        return res[1]
