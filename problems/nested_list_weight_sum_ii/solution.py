9b0707db82ee5034f19d168c2783617c9039e862
phillip055 <vorathep055@gmail.com>
2024-01-26 06:14:20 +0000

    Sync LeetCode submission - Nested List Weight Sum II (python3)

---
+++ problems/nested_list_weight_sum_ii/solution.py
@@ -0,0 +1,69 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        result = []
+        def dfs(nestedList, list_sum):
+            temp_list = []
+            for elem in nestedList:
+                if elem.isInteger():
+                    list_sum += elem.getInteger()
+                else:
+                    temp_list += elem.getList()
+            if len(temp_list) != 0:
+                list_sum+=dfs(temp_list, list_sum)
+            return list_sum
+        return dfs(nestedList, 0)
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
