d713c97c66aae50f56344476893a32ecf3fe715b
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-26 16:49:45 +0000

    Sync LeetCode submission Runtime - 158 ms (49.00%), Memory - 17.3 MB (7.59%)

--- problems/0599-minimum-index-sum-of-two-lists/solution.py
+++ problems/0599-minimum-index-sum-of-two-lists/solution.py
@@ -1,28 +1,24 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        myDict = {}
-    
-        for i in range(len(list1)):
-            for j in range(len(list2)):
-                if list1[i] == list2[j]:
-                    myDict[list1[i]] = i+j
+        
+        freq = defaultdict(list)
+        
+        for idx, v in enumerate(list1):
+            freq[v].append(idx)
+        
+        for idx, v in enumerate(list2):
+            freq[v].append(idx)
+        
+        scores = defaultdict(int)
+        
+        for k, v in freq.items():
+            if len(v) == 2:
+                scores[k] += sum(v)
+        
+        lowest = min(scores.values())
+        result = []
+        for k, v in scores.items():
+            if v == lowest:
+                result.append(k)
+        return result
 
-        #print(myDict)
-
-        minkey = min(myDict, key=myDict.get)
-
-        #print(minkey)
-
-        minvalue = myDict[minkey]
-
-        #print(minvalue)
-
-        retList = []
-
-        for key, value in myDict.items():
-            if value == minvalue:
-                retList.append(key)
-
-        #print(retList)
-
-        return retList
