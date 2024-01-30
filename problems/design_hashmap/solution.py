1bcf9aa64d0598343a4f7749b9e013261dc6de92
phillip055 <vorathep055@gmail.com>
2024-01-30 18:08:56 +0000

    Sync LeetCode submission - Design HashMap (python3)

--- problems/design_hashmap/solution.py
+++ problems/design_hashmap/solution.py
@@ -1,36 +1,21 @@
 class MyHashMap:
 
     def __init__(self):
-        self.lst = []
+        self.data = [None] * 1000001
 
     def put(self, key: int, value: int) -> None:
-        found_idx = -1
-        for idx, (k, v) in enumerate(self.lst):
-            if k == key:
-                found_idx = idx
-                break
-        if found_idx == -1:
-            self.lst.append((key, value))
-        else:
-            self.lst[found_idx] = (key, value)
+        self.data[key] = value
+        return None
 
     def get(self, key: int) -> int:
-        for (k,v) in self.lst:
-            if key == k:
-                return v
-        return -1
+        if self.data[key] != None:
+            return self.data[key]
+        else:
+            return -1
 
     def remove(self, key: int) -> None:
-        found_idx = -1
-        for idx, (k, v) in enumerate(self.lst):
-            if k == key:
-                found_idx = idx
-                break
-        if found_idx == -1:
-            return
-        self.lst[found_idx], self.lst[len(self.lst) - 1] = self.lst[len(self.lst) - 1], self.lst[found_idx]
-        self.lst.pop()
-        return
+        self.data[key] = None
+        return None
 
 
 # Your MyHashMap object will be instantiated and called as such:
