e78f1019e1d61b501f7dfa7b375687c5959c67ca
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-31 19:37:41 +0000

    Sync LeetCode submission Runtime - 45 ms (75.96%), Memory - 16.7 MB (49.71%)

--- problems/0350-intersection-of-two-arrays-ii/solution.py
+++ problems/0350-intersection-of-two-arrays-ii/solution.py
@@ -1,4 +1,11 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return [n for n in set(nums1) & set(nums2) for i in range(min(nums1.count(n), nums2.count(n)))]
-                    
+        num1_freq = Counter(nums1)
+        result = []
+        for num in nums2:
+            if num in num1_freq:
+                result.append(num)
+                num1_freq[num] -= 1
+                if num1_freq[num] == 0:
+                    del num1_freq[num]
+        return result
