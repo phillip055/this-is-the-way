4b4ae399a9e54e47dbd046bd5a1282c1b0d3036e
Vorathep Sumetphong <vorathep055@gmail.com>
2024-01-31 19:30:05 +0000

    Sync LeetCode submission Runtime - 216 ms (86.70%), Memory - 43.1 MB (95.21%)

--- problems/0111-minimum-depth-of-binary-tree/solution.py
+++ problems/0111-minimum-depth-of-binary-tree/solution.py
@@ -5,15 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minDepth(self, root: Optional[TreeNode], count = 0) -> int:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        
         if not root:
-            return count
-        if not root.left:
-            return 1 + self.minDepth(root.right, count)
-        if not root.right:
-            return 1 + self.minDepth(root.left, count)
-        return 1 + min(
-            self.minDepth(root.left, count),
-            self.minDepth(root.right, count),
-        )
-
+            return 0
+        to_explore = deque([(root, 1)])
+        
+        while len(to_explore):
+            node, level = to_explore.popleft()
+            if not node.left and not node.right:
+                return level
+            if node.left:
+                to_explore.append((node.left, level + 1))
+            if node.right:
+                to_explore.append((node.right, level + 1))
+        return 1
+        
