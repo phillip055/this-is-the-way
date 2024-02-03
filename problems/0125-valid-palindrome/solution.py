7a8923401f6b98f75b76f60f54acaeda28fed9f1
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-03 03:30:41 +0000

    Sync LeetCode submission Runtime - 40 ms (81.33%), Memory - 17 MB (63.23%)

--- problems/0125-valid-palindrome/solution.py
+++ problems/0125-valid-palindrome/solution.py
@@ -1,18 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        
         idx0, idx1 = 0, len(s) - 1
+        
         while idx0 < idx1:
-            c0 = s[idx0].lower()
-            c1 = s[idx1].lower()
-            if not c0.isnumeric() and not c0.isalpha() or c0 == " ":
+            if not s[idx0].isnumeric() and not s[idx0].isalpha():
                 idx0 += 1
                 continue
-            if not c1.isnumeric() and not c1.isalpha() or c1 == " ":
+            if not s[idx1].isnumeric() and not s[idx1].isalpha():
                 idx1 -= 1
                 continue
-            if c0 != c1:
+            if s[idx0].lower() != s[idx1].lower():
                 return False
             idx0 += 1
             idx1 -= 1
         return True
-
+        
