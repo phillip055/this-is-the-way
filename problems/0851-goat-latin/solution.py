f682c10e12812ce0562ec7ef4ca4fd95fae7c4e6
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-03 04:20:23 +0000

    Sync LeetCode submission Runtime - 43 ms (7.19%), Memory - 16.5 MB (91.31%)

--- problems/0851-goat-latin/solution.py
+++ problems/0851-goat-latin/solution.py
@@ -1,12 +1,16 @@
 class Solution:
     def toGoatLatin(self, sentence: str) -> str:
         words = sentence.split()
-        result = []
-        for idx, word in enumerate(words):
+        
+        for idx in range(len(words)):
+            word = words[idx] 
             if word[0] not in ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U"):
-                word = list(word)
-                letter = word.pop(0)
-                word = "".join(word) + letter    
-            word += "ma" + ("a" * (idx + 1))
-            result.append(word)
-        return " ".join(result)
+                first = word[0]
+                word = word[1:]
+                word += first
+            word += "ma"
+            word += "a"*(idx+1)
+            words[idx] = word
+        return " ".join(words)
+                
+
