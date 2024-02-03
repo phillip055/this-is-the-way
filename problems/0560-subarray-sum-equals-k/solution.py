a4e3cba735444cf9be7863908293a6da97b78b06
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-03 04:33:16 +0000

    Sync LeetCode submission Runtime - 237 ms (31.05%), Memory - 20.8 MB (10.01%)

--- problems/0560-subarray-sum-equals-k/solution.py
+++ problems/0560-subarray-sum-equals-k/solution.py
@@ -1,17 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        l = 0
-        count = 0
-        curr = 0
-        prev_sum = defaultdict(lambda : 0)
-        for r in range(len(nums)):
-            curr += nums[r]
-            print(l, r, curr)
-            if curr == k:
-                count += 1
-            if curr - k in prev_sum:
-                count += prev_sum[curr - k] 
-            prev_sum[curr] += 1
-        return count
-        
-        
+        prefixSums = defaultdict(lambda : 0)
+        prefixSums[0] = 1
+        currSum = 0
+        result = 0
+        for num in nums:
+            currSum += num
+            diff = currSum - k
+            result += prefixSums[diff]
+            prefixSums[currSum] += 1
+        return result
+
