d3ee32d02cda1c8deac9be6366e9a11650593062
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-05 14:29:36 +0000

    Sync LeetCode submission Runtime - 517 ms (95.43%), Memory - 77.3 MB (86.13%)

--- problems/0146-lru-cache/solution.py
+++ problems/0146-lru-cache/solution.py
@@ -1,50 +1,25 @@
-class DoublyLinkedListNode:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.next = None
-        self.prev = None
-
 class LRUCache:
     def __init__(self, capacity: int):
-        self.head = DoublyLinkedListNode(-1, -1)
-        self.tail = DoublyLinkedListNode(-1, -1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
         self.capacity = capacity
-        self.kvs = {}
+        self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key not in self.kvs:
-            return -1
-        node = self.kvs[key]
-        self.remove(node)
-        self.insert(node)
-        return node.val
-    
-    def remove(self, node):
-        del self.kvs[node.key]
-        node.prev.next = node.next
-        node.next.prev = node.prev
-    
-    def insert(self, node):
-        self.tail.prev.next = node
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev = node
-        self.kvs[node.key] = node
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.kvs:
-            node = self.kvs[key]
-            self.remove(node)
-            self.insert(DoublyLinkedListNode(key, value))
+        if key in self.cache:
+            # Update the value and move the key to the end
+            self.cache[key] = value
+            self.cache.move_to_end(key)
         else:
-            if len(self.kvs) == self.capacity:
-                self.remove(self.head.next)
-            self.insert(DoublyLinkedListNode(key, value))
-
-
+            # Add a new key-value pair
+            if len(self.cache) >= self.capacity:
+                # Evict the least recently used key (first key in OrderedDict)
+                self.cache.popitem(last=False)
+            self.cache[key] = value
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
