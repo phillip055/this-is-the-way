86e1871ad3d998a60008a9ea7719bde5b760e96a
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-05 15:03:14 +0000

    Sync LeetCode submission Runtime - 153 ms (62.65%), Memory - 21.3 MB (43.02%)

--- problems/0912-random-pick-with-weight/solution.py
+++ problems/0912-random-pick-with-weight/solution.py
@@ -1,24 +1,24 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.prefix_sum = []
+        self.w = w
+        self.prefix_sum = [0]
         self.total = 0
-        for weight in w:
-            self.total += weight
-            self.prefix_sum.append(self.total)
+        for num in w:
+            self.prefix_sum.append(self.prefix_sum[-1] + num)
+            self.total += num
+        self.prefix_sum.pop(0)
 
     def pickIndex(self) -> int:
-        pick = random.uniform(0, self.total)
-
+        v = random.uniform(0, self.total)
         l, r = 0, len(self.prefix_sum) - 1
         while l < r:
             mid = (l + r) // 2
-            if self.prefix_sum[mid] < pick:
-                l = mid + 1
-            else:
+            if self.prefix_sum[mid] >= v:
                 r = mid
+            else:
+                l = mid + 1
         return l
-
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
