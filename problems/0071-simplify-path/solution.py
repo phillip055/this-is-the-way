4cc5d71d68cfd4172adac32a956c975ff56fadb0
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-06 13:05:23 +0000

    Sync LeetCode submission Runtime - 40 ms (36.61%), Memory - 16.6 MB (85.85%)

--- problems/0071-simplify-path/solution.py
+++ problems/0071-simplify-path/solution.py
@@ -1,16 +1,23 @@
+# Algo
+# result = [home] input.split("/")
+# "/" + "/".join(result)
+# result = [..]
+# "/".join([])
+
+# O(n), O(n)
 class Solution:
     def simplifyPath(self, path: str) -> str:
         tokens = path.split("/")
         stack = []
         for token in tokens:
-            if token == "..":
+            if token == "." or token == "":
+                continue
+            elif token == "..":
                 if len(stack):
                     stack.pop()
-                else:
-                    continue
-            elif token == "." or "" == token:
-                continue
             else:
                 stack.append(token)
-        return '/' + '/'.join(stack)
+        return "/" + "/".join(stack)
 
+
+    
