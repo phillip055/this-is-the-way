233ea195cca327f53a5caee1af1c9cc006b8255d
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-08 11:07:01 +0000

    Sync LeetCode submission Runtime - 1199 ms (26.15%), Memory - 22.2 MB (5.62%)

--- problems/0015-3sum/solution.py
+++ problems/0015-3sum/solution.py
@@ -1,22 +1,21 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        
-        result = []
-        in_result = set()
-        for idx in range(len(nums) - 2):
-            total = nums[idx]
-            l, r = idx + 1, len(nums) - 1
-            while l < r:
-                candidate = total + nums[l] + nums[r]
+        result = set()
+        for idx in range(len(nums)):
+            num = nums[idx]
+            left = idx + 1
+            right = len(nums) - 1
+            while left < right:
+                candidate = num + nums[left] + nums[right]
                 if candidate == 0:
-                    if (total, nums[l], nums[r]) not in in_result:
-                        result.append([total, nums[l], nums[r]])
-                        in_result.add((total, nums[l], nums[r]))
-                    r -= 1
-                if candidate > 0:
-                    r -= 1
+                    result.add((num, nums[left], nums[right]))
+                    left += 1
+                    right -= 1
+                elif candidate < 0:
+                    left += 1
                 else:
-                    l += 1
-        return result
+                    right -= 1
+        return list(map(list, result))
+
 
