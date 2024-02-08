d8f690c180a16d12340818b9bc0fd9123fda8631
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-08 12:37:48 +0000

    Sync LeetCode submission Runtime - 38 ms (52.56%), Memory - 16.7 MB (13.25%)

--- problems/0314-binary-tree-vertical-order-traversal/solution.py
+++ problems/0314-binary-tree-vertical-order-traversal/solution.py
@@ -6,23 +6,23 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
+        if root is None:
             return []
-        q = deque([(root, 0)])
         cols = defaultdict(list)
         
+        q = deque([(root, 0)])
+        min_col, max_col = 0, 0
         while len(q):
             node, col = q.popleft()
             cols[col].append(node.val)
+            min_col = min(col, min_col)
+            max_col = max(col, max_col)
             if node.left:
                 q.append((node.left, col-1))
             if node.right:
                 q.append((node.right, col+1))
-        
-        sorted_keys = sorted(dict(cols.items()))
         result = []
-        for key in sorted_keys:
-            result.append(cols[key])
+        for col in range(min_col, max_col + 1):
+            result.append(cols[col])
         return result
-        
-
+            
