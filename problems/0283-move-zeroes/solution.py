933972c74509c37d3de4ed6247450d30808f79a8
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-08 12:32:30 +0000

    Sync LeetCode submission Runtime - 125 ms (66.14%), Memory - 18 MB (78.31%)

--- problems/0283-move-zeroes/solution.py
+++ problems/0283-move-zeroes/solution.py
@@ -1,14 +1,3 @@
-# Algorithm
-# non_zero_idx = 0
-
-# [0,1,0,3,12]
-# [1,1, 0, 3, 12] # non_zero_idx = 1
-# [1,3, 0, 3, 12] # non_zero_idx = 2
-# [1,3, 12, 3, 12] # non_zero_idx = 3
-
-# ƒor index above and equal 3, 00000
-
-
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
@@ -19,9 +8,6 @@ class Solution:
             if num != 0:
                 nums[non_zero_idx] = num
                 non_zero_idx += 1
-        
         for idx in range(non_zero_idx, len(nums)):
             nums[idx] = 0
-        return nums
-
-
+        
