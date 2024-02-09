839deba9382cfb0535e38cdd250ccf850d638a7b
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-09 16:07:24 +0000

    Sync LeetCode submission Runtime - 541 ms (8.83%), Memory - 17.2 MB (10.75%)

--- problems/0621-task-scheduler/solution.py
+++ problems/0621-task-scheduler/solution.py
@@ -1,25 +1,27 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        interval = 0
+        
         freq = Counter(tasks)
-        counts = freq.values()
-
-        t = 0
+        
         q = []
-        pq = [-c for c in counts]
+        
+        pq = [(-v, k) for k, v in freq.items()]
+        
         heapq.heapify(pq)
-
-        while len(q) or len(pq):
-            t += 1
+        
+        while len(pq) or len(q):
+            interval += 1
+            
             if len(pq):
-                node = heapq.heappop(pq)
-                node += 1
-                if node < 0:
-                    q.append((node, t + n))
-            while len(q) and q[0][1] == t:
-                node = q.pop(0)
-                heapq.heappush(pq, node[0])
-        return t
+                item = heapq.heappop(pq)
+                if item[0] + 1 < 0:
+                    q.append((item[0] + 1, item[1], interval + n))
+            
+            while len(q) and q[0][2] <= interval:
+                item = q.pop(0)
+                heapq.heappush(pq, (item[0], item[1]))
+        return interval
                 
-
-
-
+        
+        
