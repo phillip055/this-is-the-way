ff3513222128b47427aebfe559537dccc631037c
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-09 01:34:46 +0000

    Sync LeetCode submission Runtime - 132 ms (14.74%), Memory - 17.8 MB (10.69%)

--- problems/1028-interval-list-intersections/solution.py
+++ problems/1028-interval-list-intersections/solution.py
@@ -1,16 +1,26 @@
+# Algo
+# [[13,23],[24,25]]
+# [[8,12],[15,24],[25,26]]
+
+
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         idx0, idx1 = 0, 0
         result = []
         while idx0 < len(firstList) and idx1 < len(secondList):
-            if firstList[idx0][1] >= secondList[idx1][0] and firstList[idx0][0] <= secondList[idx1][1]:
+            first = firstList[idx0]
+            second = secondList[idx1]
+
+            if first[1] >= second[0] and first[0] <= second[1]:
                 result.append([
-                    max(firstList[idx0][0], secondList[idx1][0]), 
-                    min(firstList[idx0][1], secondList[idx1][1])
+                    max(first[0], second[0]),
+                    min(first[1], second[1])
                 ])
-            if firstList[idx0][1] < secondList[idx1][1]:
+            if first[1] < second[1]:
                 idx0 += 1
             else:
                 idx1 += 1
         return result
-    
+
+
+        
