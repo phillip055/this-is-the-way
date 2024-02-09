8c8fd260bc4973aecb23d0067068873099eeb436
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-09 16:41:20 +0000

    Sync LeetCode submission Runtime - 30 ms (93.73%), Memory - 16.5 MB (43.54%)

--- problems/1848-sum-of-unique-elements/solution.py
+++ problems/1848-sum-of-unique-elements/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
         freq = Counter(nums)
-        _sum = 0
+        result = 0
         for k, v in freq.items():
             if v == 1:
-                _sum += k
-        return _sum
-
+                result += k
+        return result
+        
