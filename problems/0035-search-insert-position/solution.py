5edce1a50d0f43e6ad9517b9a49625810e94f75c
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-09 11:48:42 +0000

    Sync LeetCode submission Runtime - 48 ms (66.44%), Memory - 17.2 MB (85.12%)

--- problems/0035-search-insert-position/solution.py
+++ problems/0035-search-insert-position/solution.py
@@ -1,4 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        idx = bisect_left(nums, target)
-        return idx
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
+
+
