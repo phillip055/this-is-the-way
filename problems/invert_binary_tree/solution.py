4ab1b4c58139eb4cb2fa8e229fe8cf328cb6d425
phillip055 <vorathep055@gmail.com>
2024-02-11 16:50:01 +0000

    Sync LeetCode submission - Invert Binary Tree (python3)

--- problems/invert_binary_tree/solution.py
+++ problems/invert_binary_tree/solution.py
@@ -8,6 +8,5 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if root == None:
             return None
-        tmp = root.left
         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
         return root
