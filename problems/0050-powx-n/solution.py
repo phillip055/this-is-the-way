f9f8863b21aaad049df113c140aff8be38805362
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-11 03:23:04 +0000

    Sync LeetCode submission Runtime - 34 ms (66.52%), Memory - 16.5 MB (69.88%)

--- problems/0050-powx-n/solution.py
+++ problems/0050-powx-n/solution.py
@@ -3,9 +3,6 @@ class Solution:
         if n == 0: return 1
         if n == 1: return x
         if n < 0: return 1 / self.myPow(x, -n)
-
-        if n % 2 == 0:
-            return self.myPow(x * x, n // 2)
+        if n % 2 == 0: return self.myPow(x * x, n // 2)
         return x * self.myPow(x, n - 1)
-
 
