8973d1c44c85ba00871347bc72bbb90131ae206d
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-11 04:33:14 +0000

    Sync LeetCode submission Runtime - 1331 ms (5.05%), Memory - 24.3 MB (23.38%)

--- problems/0940-fruit-into-baskets/solution.py
+++ problems/0940-fruit-into-baskets/solution.py
@@ -1,27 +1,16 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        basket = {}
-        max_picked = 0
+        max_length = 0
         left = 0
-        
-        # Add fruit from the right index (right) of the window.
+        fruit_types = defaultdict(lambda : 0)
         for right in range(len(fruits)):
-            basket[fruits[right]] = basket.get(fruits[right], 0) + 1
-            
-            # If the current window has more than 2 types of fruit,
-            # we remove fruit from the left index (left) of the window,
-            # until the window has only 2 types of fruit.
-            while len(basket) > 2:
-                basket[fruits[left]] -= 1
-                if basket[fruits[left]] == 0:
-                    del basket[fruits[left]]
+            print(fruit_types.items())
+            fruit_types[fruits[right]] += 1
+            while len(fruit_types) > 2:
+                fruit_types[fruits[left]] -= 1
+                if fruit_types[fruits[left]] == 0:
+                    del fruit_types[fruits[left]]
                 left += 1
-            
-            # Update max_picked.
-            max_picked = max(max_picked, right - left + 1)
-        
-        # Return max_picked as the maximum number of fruits we can collect.
-        return max_picked
-
-# [3,3,3,1,2,1,1,2,3,3,4]
+            max_length = max(max_length, right - left + 1)
+        return max_length
 
