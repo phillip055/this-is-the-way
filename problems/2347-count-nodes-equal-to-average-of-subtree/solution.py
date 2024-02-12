8dd7293fa86f62f9d8af554bc57b4de965c87240
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-12 18:38:31 +0000

    Sync LeetCode submission Runtime - 40 ms (75.68%), Memory - 16.8 MB (67.94%)

--- problems/2347-count-nodes-equal-to-average-of-subtree/solution.py
+++ problems/2347-count-nodes-equal-to-average-of-subtree/solution.py
@@ -6,26 +6,27 @@
 #         self.right = right
 class Solution:
     def averageOfSubtree(self, root: TreeNode) -> int:
+        
         count = 0
+
         def helper(root):
             nonlocal count
-            if not root:
-                return (0, 0)
-            ts = 0
-            tc = 0
-            avg = helper(root.left)
-            ts += avg[0]
-            tc += avg[1]
-            avg = helper(root.right)
-            ts += avg[0]
-            tc += avg[1]
-            ts += root.val
-            tc += 1
-            if (ts//tc) == root.val:
+            if root is None:
+                return (0, 0) # sum, count
+            
+            lsum, lcount = helper(root.left)
+            rsum, rcount = helper(root.right)
+            
+            tsum = root.val + lsum + rsum
+            tcount = lcount + rcount + 1
+
+            if tsum // tcount == root.val:
                 count += 1
-            return ts, tc
+
+            return tsum, tcount 
+        
         helper(root)
         return count
-        
+            
+            
 
-    
