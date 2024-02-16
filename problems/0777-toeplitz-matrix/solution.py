0f5201ca367ac8fc074139e6b1bcac9b51e6c2b5
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-16 13:05:17 +0000

    Sync LeetCode submission Runtime - 81 ms (34.50%), Memory - 16.5 MB (77.29%)

--- problems/0777-toeplitz-matrix/solution.py
+++ problems/0777-toeplitz-matrix/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        for rowIdx in range(1, len(matrix)):
-            row0 = matrix[rowIdx-1]
-            row1 = matrix[rowIdx]
-            if row0[:-1] != row1[1:]:
+        if len(matrix) <= 1:
+            return True
+        
+        for row in range(1, len(matrix)):
+            if matrix[row-1][:-1] != matrix[row][1:]:
                 return False
         return True
-            
+
 
