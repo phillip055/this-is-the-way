01a5ca6f7e736bb1c20b6c6b65f2234a05008b31
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-16 12:41:03 +0000

    Sync LeetCode submission Runtime - 31 ms (81.16%), Memory - 16.6 MB (12.13%)

--- problems/1894-merge-strings-alternately/solution.py
+++ problems/1894-merge-strings-alternately/solution.py
@@ -1,9 +1,13 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         result = ""
-        for a, b in zip(word1, word2):
-            result += a
-            result += b
-        remainingIdx = min(len(word1), len(word2))
-        return result + word1[remainingIdx:] + word2[remainingIdx:]
+        overlap_len = min(len(word1), len(word2))
+        for idx in range(overlap_len):
+            result += word1[idx]
+            result += word2[idx]
+        for idx in range(overlap_len, len(word1)):
+            result += word1[idx]
+        for idx in range(overlap_len, len(word2)):
+            result += word2[idx]
+        return result
 
