ef335f118ede00408dca33609c3c29c4f27587bf
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-16 13:42:03 +0000

    Sync LeetCode submission Runtime - 28 ms (95.47%), Memory - 16.7 MB (77.66%)

--- problems/0078-subsets/solution.py
+++ problems/0078-subsets/solution.py
@@ -1,15 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        output = []
         subset = []
 
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset.copy())
+        def helper(idx):
+            if idx > len(nums) - 1:
+                output.append(subset.copy())
                 return
-            subset.append(nums[i])
-            dfs(i + 1)
+            
+            subset.append(nums[idx])
+            helper(idx + 1)
             subset.pop()
-            dfs(i + 1)
-        dfs(0)
-        return res
+            helper(idx + 1)
+        helper(0)
+        return output
+
