134f92336778a56fbf26f9b91db84f46d81ea676
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-17 03:34:44 +0000

    Sync LeetCode submission Runtime - 910 ms (36.73%), Memory - 31.7 MB (29.96%)

--- problems/0739-daily-temperatures/solution.py
+++ problems/0739-daily-temperatures/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = [(temperatures[0], 0)]
-        output = [0] * len(temperatures)
-        for idx in range(1, len(temperatures)):
-            while len(stack) and temperatures[idx] > stack[-1][0]:
-                node = stack.pop()
-                output[node[1]] = (idx - node[1])
-            stack.append((temperatures[idx], idx))
-        return output
-        
+        result = [0] * len(temperatures)
+        stack = []
+        for idx, temp in enumerate(temperatures):
+            while len(stack) and stack[-1][0] < temp:
+                t, i = stack.pop()
+                result[i] = idx - i
+            stack.append((temp, idx))
+        return result        
+
