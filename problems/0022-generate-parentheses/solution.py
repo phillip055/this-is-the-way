4654a9a437ee560e3f69149f922ed39eb5374d28
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-18 09:02:17 +0000

    Sync LeetCode submission Runtime - 34 ms (79.54%), Memory - 16.8 MB (89.14%)

--- problems/0022-generate-parentheses/solution.py
+++ problems/0022-generate-parentheses/solution.py
@@ -1,14 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
-        def helper(opened, closed, stack, n):
-            nonlocal result
+        def helper(n, opened=0, closed=0, stack=[]):
             if closed == n:
                 result.append("".join(stack))
                 return
             if opened < n:
-                helper(opened + 1, closed, stack + ["("], n)
+                helper(n, opened + 1, closed, stack + ["("])
             if closed < opened:
-                helper(opened, closed + 1, stack + [")"], n)
-        helper(0, 0 , [], n)
+                helper(n, opened, closed + 1, stack + [")"])
+        helper(n)
         return result
+
