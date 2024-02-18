9795dfa482241136c4c3bcd40f73d43b7cb8081c
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-18 10:14:16 +0000

    Sync LeetCode submission Runtime - 38 ms (37.53%), Memory - 16.5 MB (86.71%)

--- problems/0226-invert-binary-tree/solution.py
+++ problems/0226-invert-binary-tree/solution.py
@@ -6,7 +6,8 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root == None:
+        if root is None:
             return None
         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
         return root
+
