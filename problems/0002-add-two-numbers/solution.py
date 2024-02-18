9de1c9d18eae72e36c291937b93a4995c4a15e7c
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-18 09:38:28 +0000

    Sync LeetCode submission Runtime - 46 ms (93.58%), Memory - 16.6 MB (48.33%)

--- problems/0002-add-two-numbers/solution.py
+++ problems/0002-add-two-numbers/solution.py
@@ -1,43 +1,28 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        carry = 0
+        result = dummy
+        dummy = result
+        while l1 or l2 or carry != 0:
+            _sum = 0
+            if l1:
+                _sum += l1.val
+                l1 = l1.next
+            if l2:
+                _sum += l2.val
+                l2 = l2.next
+            _sum += carry
+            newNode = ListNode(_sum % 10)
+            result.next = newNode
+            carry = _sum // 10
+            result = newNode
+        
+        return dummy.next
 
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        x1 = ''
-        currentNode = l1
-        while(currentNode != None):
-            x1 += str(currentNode.val)
-            currentNode = currentNode.next
-        x1 = x1[::-1]
-        x2 = ''
-        currentNode = l2
-        while(currentNode != None):
-            x2 += str(currentNode.val)
-            currentNode = currentNode.next
-        x2 = x2[::-1]
 
-        res = int(x1) + int(x2)
-        resString = str(res)
-        reversedResString = resString[::-1]
-        
-        latestNode = None
-        firstNode = None
-        
-        for i in range(len(reversedResString)):
-            node = ListNode(int(reversedResString[i]))
-            if latestNode == None:
-                firstNode = node
-                latestNode = node
-            else:
-                latestNode.next = node
-                latestNode = node
-        return firstNode
-        
