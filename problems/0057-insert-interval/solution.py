a20af96be093ed65ac4bac837abb23fbd78a4777
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-18 16:47:56 +0000

    Sync LeetCode submission Runtime - 79 ms (32.17%), Memory - 19.9 MB (37.43%)

--- problems/0057-insert-interval/solution.py
+++ problems/0057-insert-interval/solution.py
@@ -1,39 +1,19 @@
-# firstList = intervals
-# secondList = [newInterval]
-
-# [[1,3], [2,5], [6,9]]
-# 
-# [1,5]
-
-
-
-# [1,10]
-# [2,5]
-
-# [3,10]
-# [2,5]
-# [2,10]
-
-# Algorithm
-# mergable = check if newInterval[0] <= interval[1]
-# to_insert_interval = (min(newInterval[0], min(interval[0]), max(newInteraval[1], interval[1])))
-
-# O(n), O(n)
-
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        output = []
-        for idx in range(len(intervals)):
-            if newInterval[1] < intervals[idx][0]:
-                output.append(newInterval)
-                return output + intervals[idx:]
-            elif newInterval[0] > intervals[idx][1]:
-                output.append(intervals[idx])
-            else:
-                newInterval = [
-                    min(newInterval[0], intervals[idx][0]),
-                    max(newInterval[1], intervals[idx][1])
-                ]
-        output.append(newInterval)
-        return output
+        position = bisect.bisect_left(intervals, newInterval) 
+        intervals.insert(position, newInterval)
+
+        def mergeIntervals(intervals):
+            if not len(intervals):
+                return []
+            result = [intervals[0]]
+            for intervalIdx in range(1, len(intervals)):
+                interval = intervals[intervalIdx]
+                if interval[0] <= result[-1][1]:
+                    result[-1][1] = max(interval[1], result[-1][1])
+                else:
+                    result.append(interval)
+            return result
+        
+        return mergeIntervals(intervals)
 
