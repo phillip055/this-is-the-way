9f0f4b3adbe93cc06cdf2f11c44178a740eda9a7
Vorathep Sumetphong <vorathep055@gmail.com>
2024-02-21 15:11:17 +0000

    Sync LeetCode submission Runtime - 41 ms (29.34%), Memory - 17 MB (84.95%)

--- problems/0758-convert-binary-search-tree-to-sorted-doubly-linked-list/solution.py
+++ problems/0758-convert-binary-search-tree-to-sorted-doubly-linked-list/solution.py
@@ -11,27 +11,23 @@ class Solution:
     def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if root is None:
             return None
-        
         first, last = None, None
-
+        
         def helper(root):
             nonlocal first, last
             if root is None:
                 return
             helper(root.left)
-
             if last:
                 last.right = root
                 root.left = last
             else:
                 first = root
-            
-            last = root
+            last = root                
             helper(root.right)
         
         helper(root)
         first.left = last
         last.right = first
         return first
-                
-
+        
