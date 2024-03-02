5a5494b3dd4e6bd7f4925830137b3ba1a2a1386f
phillip055 <vorathep055@gmail.com>
2024-03-02 09:42:06 +0000

    Sync LeetCode submission - Convert Binary Search Tree to Sorted Doubly Linked List (python3)

--- problems/convert_binary_search_tree_to_sorted_doubly_linked_list/solution.py
+++ problems/convert_binary_search_tree_to_sorted_doubly_linked_list/solution.py
@@ -11,23 +11,21 @@ class Solution:
     def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if root is None:
             return None
-        
         first, last = None, None
-
+        
         def helper(root):
             nonlocal first, last
             if root is None:
                 return
             helper(root.left)
-
             if last:
-                root.left = last
                 last.right = root
+                root.left = last
             else:
                 first = root
             last = root
             helper(root.right)
-        
+    
         helper(root)
         first.left = last
         last.right = first
