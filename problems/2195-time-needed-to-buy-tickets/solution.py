cc48907933e3d0377783590385598db871249107
Vorathep Sumetphong <vorathep055@gmail.com>
2024-04-09 17:39:22 +0000

    Sync LeetCode submission Runtime - 55 ms (8.59%), Memory - 16.6 MB (50.15%)

---
+++ problems/2195-time-needed-to-buy-tickets/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        queue = deque()
+        for i in range(len(tickets)):
+            queue.append(i)
+
+        time = 0
+        while queue:
+            time += 1
+            front = queue.popleft()
+            tickets[front] -= 1
+            if k == front and tickets[front] == 0:
+                return time
+            if tickets[front] != 0:
+                queue.append(front)
+        return time
