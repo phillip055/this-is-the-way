7cc98ae442f1141719c12e7dc1e286386752bccf
Vorathep Sumetphong <vorathep055@gmail.com>
2024-05-16 02:34:48 +0000

    Sync LeetCode submission Runtime - 52 ms (23.93%), Memory - 17 MB (11.36%)

---
+++ problems/2416-evaluate-boolean-binary-tree/solution.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        if root.left is None and root.right is None:
+            return root.val
+        l = self.evaluateTree(root.left)
+        r = self.evaluateTree(root.right)
+        match root.val:
+            case 2:
+                return l or r
+            case 3:
+                return l and r
