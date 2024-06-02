fdf59b306d7560ed3584a919469e2da8a865ea1f
Vorathep Sumetphong <vorathep055@gmail.com>
2024-06-02 06:25:58 +0000

    Sync LeetCode submission Runtime - 175 ms (23.93%), Memory - 20.9 MB (69.97%)

--- problems/0344-reverse-string/solution.py
+++ problems/0344-reverse-string/solution.py
@@ -3,4 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
+
+        for i in range(len(s) // 2):
+            j = len(s) - 1 - i
+            s[i], s[j] = s[j], s[i]
+        return s
+
